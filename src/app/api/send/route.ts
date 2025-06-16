62b6bcd324f30a4072c3a9f23ec8639228afae90
Baljan Kanatbek <baljann19@gmail.com>
2025-06-17 01:20:03 +0300

    chore: remove unnecessary whitespace in route.ts for cleaner code

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
 console.log("RESEND_API_KEY at runtime:", process.env.RESEND_API_KEY);
-
+  
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
