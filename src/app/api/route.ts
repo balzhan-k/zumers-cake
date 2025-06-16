d9d66b62d3731a8f8a1a2dcd4716f30d8a36361a
Baljan Kanatbek <baljann19@gmail.com>
2025-06-17 00:42:03 +0300

    refactor: update email template for improved clarity by enhancing field labels and reorganizing content structure

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -12,14 +12,14 @@ export async function POST(req: NextRequest) {
       <p><strong>Özel Gün:</strong> ${data.occasion} - ${data.otherOccasionDetails}</p>
       <p><strong>Kişi Sayısı:</strong> ${data.servings} - ${data.otherServingsDetails}</p>
       <p><strong>Pasta Türü:</strong> ${data.cakeType} - ${data.otherCakeTypeDetails}</p>
-      <p><strong>İçerik:</strong> ${data.filling} - ${data.otherFillingDetails}</p>
+      <p><strong>Pasta İçeriği, Kat Arası Malzemeler:</strong> ${data.filling} - ${data.otherFillingDetails}</p>
       <p><strong>Renkler:</strong> ${data.colors} - ${data.otherColorsDetails}</p>
-      <p><strong>Alerjiler:</strong> ${data.allergies}</p>
-      <p><strong>Not:</strong> ${data.cakeNote}</p>
-      <p><strong>Ek İstekler:</strong> ${data.specialRequests}</p>
-      <p><strong>İsim:</strong> ${data.nameSurname}</p>
+      <p><strong>Alerji durumu:</strong> ${data.allergies}</p>
+      <p><strong>Pasta üzerine yazılacak not:</strong> ${data.cakeNote}</p>
+      <p><strong>Özel İstekler:</strong> ${data.specialRequests}</p>
+      <p><strong>İsim Soyisim:</strong> ${data.nameSurname}</p>
       <p><strong>Telefon:</strong> ${data.phone}</p>
-      <p><strong>Teslimat Tarihi:</strong> ${data.deliveryDate}</p>
+      <p><strong>Teslimat Tarihi ve Saati:</strong> ${data.deliveryDate}</p>
     `;
 
     const result = await resend.emails.send({
